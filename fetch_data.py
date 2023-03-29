1676d591ff8c926170182e726daff1cf6d709bfb
Veronica Zanella <zanella.veronica@outlook.it>
2023-03-29 16:48:12 +0300

    added types and informative prints to show what files are read

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,18 @@
-import pandas as pd
+from pandas import DataFrame, read_csv
 
-may_df = pd.read_csv(r"https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv")
-june_df = pd.read_csv(r"https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv")
-july_df = pd.read_csv(r"https://dev.hsl.fi/citybikes/od-trips-2021/2021-07.csv")
-stations_df = pd.read_csv(r"https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv")
-print(may_df.head())
+# Read files
+may_trips: DataFrame = read_csv(r"https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv")
+print("Read may trips")
+print(may_trips.head())
+
+june_trips: DataFrame = read_csv(r"https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv")
+print("Read june trips")
+print(june_trips.head())
+
+july_trips: DataFrame = read_csv(r"https://dev.hsl.fi/citybikes/od-trips-2021/2021-07.csv")
+print("Read july trips")
+print(july_trips.head())
+
+stations: DataFrame = read_csv(r"https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv")
+print("Read stations")
+print(stations.head())
