3df72e12a1f05b6df0578f78178fb395dc764063
Veronica Zanella <zanella.veronica@outlook.it>
2023-04-04 10:05:32 +0300

    Inserted check about: nullable, format and validity of station id in validation schemas. Corrected types.

--- fetch_data.py
+++ fetch_data.py
@@ -24,15 +24,15 @@ trips: DataFrame = concat([may_trips, june_trips, july_trips], ignore_index=True
 
 # Validate data
 trips_schema = DataFrameSchema({
-    'Departure_datetime': Column(str),
-    'Return_datetime': Column(str),
-    'Departure_station_id': Column(int),
-    'Return_station_id': Column(int),
+    'Departure_datetime': Column(str, Check.str_matches("^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")),
+    'Return_datetime': Column(str, Check.str_matches("^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")),
+    'Departure_station_id': Column(int, Check.isin(stations['ID']), nullable=False),
+    'Return_station_id': Column(int, Check.isin(stations['ID']), nullable=False),
     'Covered_distance_in_m': Column(float, Check.greater_than_or_equal_to(10.0)),
     'Duration_in_s': Column(int, Check.greater_than_or_equal_to(10))
 })
 stations_schema = DataFrameSchema({
-    'ID': Column(int),
+    'ID': Column(int, nullable=False),
     'Name_fi': Column(str),
     'Name_sv': Column(str),
     'Name_eng': Column(str),
@@ -41,9 +41,9 @@ stations_schema = DataFrameSchema({
     'City_fi': Column(str),
     'City_sv': Column(str),
     'Operator': Column(str),
-    'Capacity': Column(str),
-    'x': Column(str),
-    'y': Column(str)
+    'Capacity': Column(int),
+    'x': Column(float),
+    'y': Column(float)
 })
 
 # Eliminate redundant or obsolete information
@@ -72,6 +72,7 @@ finally:
 print(trips.columns)
 print("Types: ", trips.dtypes)
 print(stations.columns)
+print("Types: ", stations.dtypes)
 
 print(trips.loc[trips['Covered_distance_in_m'] < 10.0])
 print(trips.loc[trips['Duration_in_s'] < 10])
