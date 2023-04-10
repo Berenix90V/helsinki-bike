307593c822c37807feb1ef5485247fa143078cd5
Veronica Zanella <zanella.veronica@outlook.it>
2023-04-10 18:43:13 +0300

    basic frontend table with fake data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JourneysTable from "./components/JourneysTable";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <h1>Journeys table</h1>
-
       </header>
+        <JourneysTable/>
     </div>
   );
 }
