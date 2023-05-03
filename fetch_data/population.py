64e3dab62184e2df735d3f9f23dd211f0c6799a3
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-03 11:51:16 +0300

    Changed nullable conditions in trips table so that a trip must always have a start and an end

--- fetch_data/population.py
+++ fetch_data/population.py
@@ -48,12 +48,12 @@ def create_tables(engine: Engine) -> None:
     trips = Table(
         'trips', meta,
         Column('ID', Integer, primary_key=True),
-        Column('Departure_datetime', DateTime),
-        Column('Return_datetime', DateTime),
+        Column('Departure_datetime', DateTime, nullable=False),
+        Column('Return_datetime', DateTime, nullable=False),
         Column('Departure_station_ID', Integer, ForeignKey("stations.ID"), nullable=False),
         Column('Return_station_ID', Integer, ForeignKey('stations.ID'), nullable=False),
-        Column('Covered_distance', Float, CheckConstraint('"Covered_distance">=10')),
-        Column('Duration', Integer, CheckConstraint('"Duration">=10'))
+        Column('Covered_distance', Float, CheckConstraint('"Covered_distance">=10'), nullable=False),
+        Column('Duration', Integer, CheckConstraint('"Duration">=10'), nullable=False)
     )
     stations = Table(
         'stations', meta,
@@ -73,7 +73,7 @@ def create_tables(engine: Engine) -> None:
     meta.create_all(engine)
 
 
-def populate_table(engine: Engine, df: DataFrame, table: str, index=True) -> None:
+def populate_table(engine: Engine, df: DataFrame, table: str, index: bool) -> None:
     """
     Given the connection, the data and the table, it populates the table with the data through a copy.
     If the index is True one more column that keep the row number will be inserted, otherwise not.
