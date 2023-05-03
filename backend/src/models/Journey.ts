5c506e2bb47468faeb4415887698e078abcc81a8
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-03 12:27:56 +0300

    Updated tests with correct types

--- backend/src/models/Journey.ts
+++ backend/src/models/Journey.ts
@@ -18,11 +18,11 @@ export class Journey extends BaseEntity{
     })
     declare Return_datetime: Date
 
-    @ManyToOne(type => Station, station => station.Departure_journeys)
+    @ManyToOne(() => Station, station => station.Departure_journeys)
     @JoinColumn({name:"Departure_station_ID"})
     declare Departure_station:Station
 
-    @ManyToOne(type => Station, station => station.Return_journeys)
+    @ManyToOne(() => Station, station => station.Return_journeys)
     @JoinColumn({name:"Return_station_ID"})
     declare Return_station: Station
 
