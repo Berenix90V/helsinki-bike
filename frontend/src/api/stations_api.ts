114246039b323ff89a42f4e5cd0f95cd8387fc67
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-04 23:08:23 +0300

    implemented api for single station details

--- frontend/src/api/stations_api.ts
+++ frontend/src/api/stations_api.ts
@@ -1,9 +1,10 @@
 import http from "./http_common"
+import {Station} from "../interfaces/Station";
 
 export function getAllStations(){
     return http.get("stations/")
 }
 
 export function  getStationByID(id:number){
-    return http.get("stations/:id")
+    return http.get<{station: Station}>("stations/"+id)
 }
