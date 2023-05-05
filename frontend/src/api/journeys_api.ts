7664fda89902def26b1f48f0ffb374080fe437ec
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-05 10:31:01 +0300

    implemented api to get journeys from and to stations

--- frontend/src/api/journeys_api.ts
+++ frontend/src/api/journeys_api.ts
@@ -3,3 +3,11 @@ import http from "./http_common"
 export function getAllJourneys(take: number){
     return http.get("journeys/", {params:{take:take}})
 }
+
+export function getJourneysFromStation(id:number){
+    return http.get<{njourneys:number}>("journeys/from/"+id)
+}
+
+export function getJourneysToStation(id:number){
+    return http.get<{njourneys:number}>("journeys/to/"+id)
+}
