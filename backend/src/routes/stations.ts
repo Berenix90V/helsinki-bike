4e994f23eedec3fa327ae9ee1f2ac9dff3361ba8
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-09 15:11:38 +0300

    implemented station route to get paginated stations and tested

--- backend/src/routes/stations.ts
+++ backend/src/routes/stations.ts
@@ -1,18 +1,25 @@
 import express from "express";
-import {getAllStations, getStationByID} from "../controllers/stations";
+import {getAllStations, getPaginatedStations, getStationByID} from "../controllers/stations";
 import {Station} from "../models/Station";
 const router = express.Router()
 
 router.route("/").get(async(req, res) => {
-    await getAllStations()
+    const take:number = parseInt(req.query.take as string)
+    const skip:number = parseInt(req.query.skip as string)
+    await getPaginatedStations(skip, take)
         .then((allStations: Station[]) =>
             res.status(200).json({
                 stations: allStations
         }))
-        .catch((err: any)=>{
-            res.status(404).json({
-                err: err.message
-            })
+        .catch((err) => {
+            if (err instanceof RangeError || err instanceof TypeError)
+                res.status(400).json({
+                    err: err.message
+                })
+            else
+                res.status(404).json({
+                    err: err.message
+                })
         })
 })
 
