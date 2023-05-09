eb47d7ce9810d6e91f8d8977f8b596b6804712a9
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-09 19:54:59 +0300

    implemented api for getting paginated stations

--- frontend/src/api/stations_api.ts
+++ frontend/src/api/stations_api.ts
@@ -5,6 +5,10 @@ export function getAllStations(){
     return http.get("stations/")
 }
 
+export function getPaginatedStations(skip:number, take: number){
+    return http.get("stations/", {params:{skip:skip, take:take}})
+}
+
 export function  getStationByID(id:number){
     console.log(id)
     return http.get<{station: Station}>("stations/"+id)
