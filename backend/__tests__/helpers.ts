321c061151d629ba9179efc6259c07af83738d55
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-09 14:34:15 +0300

    implemented paginated method for Station entity and tested

--- backend/__tests__/helpers.ts
+++ backend/__tests__/helpers.ts
@@ -15,6 +15,11 @@ export async function get_nth_journey_id(skip:number){
     return +res[0].ID
 }
 
+export async function get_nth_station_id(skip:number){
+    const res = await AppDataSource.query(`SELECT "ID" FROM stations ORDER BY "ID" LIMIT 1 OFFSET $1`, [skip])
+    return +res[0].ID
+}
+
 export async function totJourneys(){
     const res = await AppDataSource.query(`SELECT count(*) FROM trips`)
     return +res[0].count
