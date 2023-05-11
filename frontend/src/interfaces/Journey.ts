0b2dd8c3d87869dea7b4e0411867175cfd9b93fa
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-11 12:47:53 +0300

    added columns for departure and return time

--- frontend/src/interfaces/Journey.ts
+++ frontend/src/interfaces/Journey.ts
@@ -1,6 +1,8 @@
 import {Station} from "./Station";
 
 export interface Journey{
+    Departure_datetime: string,
+    Return_datetime:string,
     Departure_station: Station,
     Return_station: Station,
     Covered_distance: number,
