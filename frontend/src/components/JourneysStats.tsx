c8633a30ae057298a54254acaae4ad73fb88e2fd
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-12 13:21:48 +0300

    implemented component to get journeys stats

---
+++ frontend/src/components/JourneysStats.tsx
@@ -0,0 +1,81 @@
+import {Col, Row} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import {
+    avgJourneyFromStation,
+    avgJourneyToStation,
+    getNJourneysFromStation,
+    getNJourneysToStation
+} from "../api/journeys_api";
+
+type JourneysParameters = {
+    id: number
+}
+function JourneysStats({id}: JourneysParameters){
+    const [nJourneysFrom, setNJourneysFrom] = useState<number>(0)
+    const [nJourneysTo, setNJourneysTo] = useState<number>(0)
+    const [avgJourneyFrom, setAvgJourneyFrom] = useState<number>(0)
+    const [avgJourneyTo, setAvgJourneyTo] = useState<number>(0)
+    useEffect(()=>{
+        const getNJourneysFrom = async () => {
+            return await getNJourneysFromStation(id)
+                .then((response) => response.data.njourneys)
+                .catch((err)=>err)
+        }
+        const getNJourneysTo = async () =>{
+            return await getNJourneysToStation(id)
+                .then((response) => response.data.njourneys)
+                .catch((err) => err)
+        }
+        const getAvgJourneyFromStation = async () => {
+            return await avgJourneyFromStation(id)
+                .then((response)=> response.data.avg)
+                .catch((err)=>err)
+        }
+
+        const getAvgJourneyToStation = async () => {
+            return await avgJourneyToStation(id)
+                .then((response)=> response.data.avg)
+                .catch((err)=>err)
+        }
+        getNJourneysFrom()
+            .then(setNJourneysFrom)
+            .catch((err)=>err)
+        getNJourneysTo()
+            .then(setNJourneysTo)
+            .catch((err)=>err)
+        getAvgJourneyFromStation()
+            .then(setAvgJourneyFrom)
+            .catch((err) => err)
+        getAvgJourneyToStation()
+            .then(setAvgJourneyTo)
+            .catch((err)=>err)
+    }, [id])
+    return(
+        <>
+            <Row>
+                <Col sm={6}>
+                    <p><b>Journeys from station</b></p>
+                    <p>{nJourneysFrom}</p>
+                </Col>
+                <Col sm={6}>
+                    <p><b>Journeys to station</b></p>
+                    <p>{nJourneysTo}</p>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col sm={6}>
+                    <p><b>Average distance</b></p>
+                    <p>{(avgJourneyFrom/1000).toFixed(2)}</p>
+                </Col>
+                <Col sm={6}>
+                    <p><b>Average distance</b></p>
+                    <p>{(avgJourneyTo/1000).toFixed(2)}</p>
+                </Col>
+            </Row>
+
+        </>
+    )
+}
+
+export {JourneysStats}
