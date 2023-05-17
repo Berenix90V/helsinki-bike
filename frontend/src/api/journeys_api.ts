0d9fa7429dbc83a1ba5d49f9a2a98c8288f7158d
Veronica Zanella <zanella.veronica@outlook.it>
2023-05-17 10:23:31 +0300

    inserted types on api response

--- frontend/src/api/journeys_api.ts
+++ frontend/src/api/journeys_api.ts
@@ -1,17 +1,18 @@
 import http from "./http_common"
 import {Destination} from "../interfaces/Destination";
 import {Departure} from "../interfaces/Departure";
+import {Journey} from "../interfaces/Journey";
 
 export function getAllJourneys(take: number){
     return http.get("journeys/", {params:{take:take}})
 }
 
 export function getPaginatedJourneys(skip:number, take: number){
-    return http.get("journeys/", {params:{skip:skip, take:take}})
+    return http.get<{journeys:Journey[]}>("journeys/", {params:{skip:skip, take:take}})
 }
 
 export function countJourneys(){
-    return http.get("journeys/count")
+    return http.get<{count:number}>("journeys/count")
 }
 export function getNJourneysFromStation(id:number, month?:string){
     if(month==="")
@@ -27,26 +28,26 @@ export function getNJourneysToStation(id:number, month?:string){
         return http.get<{count:number}>("journeys/to/"+id+"/?month="+parseInt(month as string))
 }
 
-export function getPaginatedJourneysByDepartureStation(skip:number, take: number, patternFrom:string, patternTo:string){
-    return http.get("journeys/search/", {params:{skip:skip, take:take, patternFrom:patternFrom, patternTo: patternTo}})
+export function getPaginatedJourneysBySearch(skip:number, take: number, patternFrom:string, patternTo:string){
+    return http.get<{journeys:Journey[]}>("journeys/search/", {params:{skip:skip, take:take, patternFrom:patternFrom, patternTo: patternTo}})
 }
 
 export function countJourneysWithDepartureStationStartingWith(patternFrom:string, patternTo:string){
-    return http.get("journeys/count/search/", {params:{patternFrom:patternFrom, patternTo: patternTo}})
+    return http.get<{count:number}>("journeys/count/search/", {params:{patternFrom:patternFrom, patternTo: patternTo}})
 }
 
 export function avgJourneyFromStation(id:number, month:string){
     if(month==="")
-        return http.get("journeys/from/"+id+"/distance/avg")
+        return http.get<{avg:number}>("journeys/from/"+id+"/distance/avg")
     else
-        return http.get("journeys/from/"+id+"/distance/avg?month="+month)
+        return http.get<{avg:number}>("journeys/from/"+id+"/distance/avg?month="+month)
 }
 
 export function avgJourneyToStation(id:number, month:string){
     if(month==="")
-        return http.get("journeys/to/"+id+"/distance/avg")
+        return http.get<{avg:number}>("journeys/to/"+id+"/distance/avg")
     else
-        return http.get("journeys/to/"+id+"/distance/avg?month="+month)
+        return http.get<{avg:number}>("journeys/to/"+id+"/distance/avg?month="+month)
 }
 
 export function topNDestinations(id:number, limit:number, month?:string){
